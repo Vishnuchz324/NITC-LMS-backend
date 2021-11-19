ab383d36b64df85facfd4b278bb520b878de9d97
Pavithra-Rajan <pavithra.rajan01@gmail.com>
2021-11-19 16:04:48 +0400

    added all tables

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,41 +1,113 @@
-DROP TABLE IF EXISTS student;
-DROP TABLE IF EXISTS staff;
-DROP TABLE IF EXISTS librarian;
-DROP TABLE IF EXISTS member;
-
-CREATE TABLE member (
-  user_id VARCHAR(9) PRIMARY KEY,
-  name VARCHAR(35) NOT NULL,
-  password TEXT NOT NULL,
-  phone VARCHAR(10) NOT NULL UNIQUE,
-  email VARCHAR(45) NOT NULL UNIQUE,
-  dept VARCHAR(5) NOT NULL,
-  role VARCHAR(9) NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE student(
-  roll_no VARCHAR(9),
-  validity DATE,
-  programme VARCHAR(6),
-  FOREIGN KEY (roll_no) REFERENCES member(user_id)
+CREATE TABLE members(
+user_ID CHAR(9),
+mem_name VARCHAR(35),
+pwd VARCHAR(20),
+phone CHAR(10), 
+email VARCHAR(45), 
+dept VARCHAR(5), 
+PRIMARY KEY(user_ID)
 );
 
 CREATE TABLE staff(
-  employee_id VARCHAR(9),
-  Tflag BOOLEAN,
-  FOREIGN KEY (employee_id) REFERENCES member(user_id)
+employee_ID CHAR(9),
+Tflag BOOLEAN, 
+PRIMARY KEY(employee_ID), 
+FOREIGN KEY(employee_ID) REFERENCES members(user_ID)
 );
 
-CREATE TABLE librarian (
-  employee_id INTEGER PRIMARY KEY,
-  name VARCHAR(35) NOT NULL,
-  password VARCHAR(20) NOT NULL,
-  phone VARCHAR(10) NOT NULL UNIQUE,
-  email VARCHAR(45) NOT NULL UNIQUE,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+CREATE TABLE student(
+roll_No CHAR(9),
+validity DATE, 
+programme VARCHAR(6), 
+PRIMARY KEY(roll_No), 
+FOREIGN KEY(roll_No) REFERENCES members(user_ID));
+
+CREATE TABLE librarian(
+employee_ID CHAR(9), 
+lib_name VARCHAR(25), 
+pwd VARCHAR(20), 
+phone CHAR(10), 
+email VARCHAR(45), 
+PRIMARY KEY(employee_ID));
+
+CREATE TABLE fine(
+fine_ID serial,
+user_ID CHAR(9),
+admin_ID CHAR(9),
+fine_status BOOLEAN,
+amount DECIMAL(8,2),
+payment_date DATE, 
+PRIMARY KEY(fine_ID),
+FOREIGN KEY(user_ID) REFERENCES members(user_ID), 
+FOREIGN KEY(admin_ID) REFERENCES librarian(employee_ID));
+
+CREATE TABLE request(
+request_ID serial,
+user_ID CHAR(9),
+book_name VARCHAR(35),
+req_date DATE,
+req_type BOOLEAN,
+PRIMARY KEY(request_ID),
+FOREIGN KEY(user_ID) REFERENCES members(user_ID)
 );
 
-SELECT version();
+CREATE TABLE book_details(
+ISBN CHAR(13),
+book_name VARCHAR(35),
+publisher VARCHAR(25),
+PRIMARY KEY(ISBN)
+);
+
+CREATE TABLE author(
+author_ID serial,
+author_name VARCHAR(25),
+PRIMARY KEY (author_ID)
+);
+
+CREATE TABLE book_authors(
+author_ID serial,
+ISBN CHAR(13),
+PRIMARY KEY(author_ID,ISBN),
+FOREIGN KEY (author_ID) REFERENCES author(author_ID),
+FOREIGN KEY (ISBN) REFERENCES book_details(ISBN)
+);
+
+CREATE TABLE tags(
+tag_ID serial,
+tag_name VARCHAR(10),
+PRIMARY KEY (tag_ID)
+);
+
+CREATE TABLE books_tags(
+tag_ID serial,
+ISBN CHAR(13),
+PRIMARY KEY(tag_ID,ISBN),
+FOREIGN KEY (tag_ID) REFERENCES tags(tag_ID),
+FOREIGN KEY (ISBN) REFERENCES book_details(ISBN)
+);
+
+CREATE TABLE book(
+book_number INTEGER,
+ISBN CHAR(13),
+status BOOLEAN,
+admin_ID CHAR(9),
+arrival_date DATE,
+PRIMARY KEY (book_number,ISBN),
+FOREIGN KEY (ISBN) REFERENCES book_details(ISBN),
+FOREIGN KEY(admin_ID) REFERENCES librarian(employee_ID)
+);
+
+CREATE TABLE borrowal(
+issue_ID serial,
+user_ID CHAR(9),
+admin_ID CHAR(9),
+ISBN CHAR(13),
+book_number INTEGER,
+renewals_left INTEGER,
+issue_date INTEGER,
+PRIMARY KEY(issue_ID),
+FOREIGN KEY(user_ID) REFERENCES members(user_ID),
+FOREIGN KEY(admin_ID) REFERENCES librarian(employee_ID),
+FOREIGN KEY (ISBN) REFERENCES book_details(ISBN),
+FOREIGN KEY (book_number) REFERENCES book(book_number)
+);
